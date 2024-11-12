772bb9d59d6947ba9ec8591d25667230bf2d77a7
luis.ladron <luis.ladron@aoshield.com>
2024-11-12 09:49:45 -0600

    test1

--- src/weather-tracker-client/src/theme/index.ts
+++ src/weather-tracker-client/src/theme/index.ts
@@ -3,7 +3,7 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#9c27b0",
+      main: "#1976d2",
       light: "#42a5f5",
       dark: "#1565c0",
     },
